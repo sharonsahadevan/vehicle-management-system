93f886c70325feb23da32a67efac728c2036672f
sharon sahadevan <sharonsujee36@gmail.com>
2018-12-07 16:32:48 +0530

    Add project files.

---
+++ vms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vms.MvcApplication" Language="C#" %>
---
+++ vms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vms.Startup))]
+namespace vms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
